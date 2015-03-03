f3530d0c97006baf36ca37f1a57ad15e72226ffa
Dana Mulder <danarmulder@gmail.com>
2015-03-03 10:19:30 -0800

    add form and Base64 encoding

--- app/controllers/registration_controller.rb
+++ app/controllers/registration_controller.rb
@@ -1,12 +1,17 @@
 class RegistrationController < ApplicationController
   require 'rest-client'
   require 'json'
+  require 'base64'
 
   def index
-    @response = RestClient.post "http://gfeelitdev.elasticbeanstalk.com/checkemail/", {:email => 'aGdmaGdnaG1j'}
-    @status = JSON.parse(@response.body)
 
   end
 
+  def search
+    email = params[:e].downcase
+    enc_email = Base64.encode64(email)
+    @response = RestClient.post "http://gfeelitdev.elasticbeanstalk.com/checkemail/", {:email => enc_email}
+    @status = JSON.parse(@response.body)
+  end
 
 end
