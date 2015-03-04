2b95fd3b5815530fbb3e1132c6cd5561271f98f2
Julien Mayer <jalexismayer@gmail.com>
2015-03-04 14:09:14 -0800

    add responsive email icon

--- app/assets/javascripts/registration.js
+++ app/assets/javascripts/registration.js
@@ -2,46 +2,56 @@ $(document).ready(function(){
   $("#registration_username").on ('blur', function(){
     var username = $('#registration_username').val();
     var re = /^[\w_]+[\w._]*[\w_]$/;
-    if (username === "") {
-      $("#registration_username").removeClass("username_blue")
-      $("#registration_username").removeClass("username_red")
-      $("#registration_username").addClass("username_white")
+    if (username === ""){
+      $("#registration_username").removeClass("username_blue");
+      $("#registration_username").removeClass("username_red");
+      $("#registration_username").addClass("username_white");
     } else if (re.test(username)){
       var enc_username = window.btoa(username);
       $.ajax("/middleman/?username=" + enc_username).done(function(response){
         var registered = response["isRegistered"];
         if(registered === 0){
-          $("#registration_username").removeClass("username_white")
-          $("#registration_username").removeClass("username_blue")
-          $("#registration_username").addClass("username_red")
+          $("#registration_username").removeClass("username_white");
+          $("#registration_username").removeClass("username_blue");
+          $("#registration_username").addClass("username_red");
         }else {
-          $("#registration_username").removeClass("username_white")
-          $("#registration_username").removeClass("username_red")
-          $("#registration_username").addClass("username_blue")
+          $("#registration_username").removeClass("username_white");
+          $("#registration_username").removeClass("username_red");
+          $("#registration_username").addClass("username_blue");
         }
       });
-    } else {
-      $("#registration_username").removeClass("username_white")
-      $("#registration_username").removeClass("username_blue")
-      $("#registration_username").addClass("username_red")
+    }else {
+      $("#registration_username").removeClass("username_white");
+      $("#registration_username").removeClass("username_blue");
+      $("#registration_username").addClass("username_red");
     }
   });
 
   $("#registration_email").on ('blur', function(){
     var email = $('#registration_email').val();
     var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    if (re.test(email)){
+    if (email === ""){
+      $("#registration_email").removeClass("email_blue");
+      $("#registration_email").removeClass("email_red");
+      $("#registration_email").addClass("email_white");
+    }else if(re.test(email)){
       var enc_email = window.btoa(email);
       $.ajax("/middleman/?email=" + enc_email).done(function(response){
         var registered = response["isRegistered"];
         if(registered === 0){
-          console.log("Not registered!");
+          $("#registration_email").removeClass("email_white");
+          $("#registration_email").removeClass("email_blue");
+          $("#registration_email").addClass("email_red");
         }else {
-          console.log("Registered!");
+          $("#registration_email").removeClass("email_white");
+          $("#registration_email").removeClass("email_red");
+          $("#registration_email").addClass("email_blue");
         }
       });
-    } else {
-      console.log("Email is not valid");
+    }else {
+      $("#registration_email").removeClass("email_white");
+      $("#registration_email").removeClass("email_blue");
+      $("#registration_email").addClass("email_red");
     }
   });
 
