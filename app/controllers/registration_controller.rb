46e66fe7049d1efd248d0994a4b5c8f2c4405512
Julien Mayer <jalexismayer@gmail.com>
2015-03-08 15:38:47 -0700

    partially functinal SDK

--- app/controllers/registration_controller.rb
+++ app/controllers/registration_controller.rb
@@ -11,7 +11,6 @@ class RegistrationController < ApplicationController
 
   def facebook
     if request.post?
-      binding.pry
       # facebook info
       authData = params["authData"]
       facebookId = Base64.encode64(authData["userID"])
@@ -20,10 +19,9 @@ class RegistrationController < ApplicationController
       userId = Base64.encode64(cookies["user_id"])
       refUsername = Base64.encode64(cookies["ref_username"])
       # query linkfbid service
-      binding.pry
       response = RestClient.post "http://gfeelitdev.elasticbeanstalk.com/linkfacebookid/", { user_id: userId, access_key: accessKey, facebookId: facebookId, ref_username: "ZGF2aWRkYW5n"}
-      # render json response, used in js to know which page to redirect to
-      binding.pry
+      json = JSON.parse(response)
+      render json: json
     else
     end
   end
