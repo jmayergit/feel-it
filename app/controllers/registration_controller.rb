749a37f2f23bf8a1bb35fb76a5d3ce21ffbf013b
Julien Mayer <jalexismayer@gmail.com>
2015-03-09 11:33:24 -0700

    debug

--- app/controllers/registration_controller.rb
+++ app/controllers/registration_controller.rb
@@ -14,10 +14,18 @@ class RegistrationController < ApplicationController
       # facebook info
       authData = params["authData"]
       facebookId = Base64.encode64(authData["userID"])
+      puts "FACEBOOKID"
+      puts facebookId
       # registration info
       accessKey = Base64.encode64(cookies["access_key"])
+      puts "ACCESSKEY"
+      puts accessKey
       userId = Base64.encode64(cookies["user_id"])
+      puts "USERID"
+      puts userId
       refUsername = Base64.encode64(cookies["ref_username"])
+      puts "REFUSERNAME"
+      puts refUsername
       # query linkfbid service
       response = RestClient.post "http://gfeelitdev.elasticbeanstalk.com/linkfacebookid/", { user_id: userId, access_key: accessKey, facebookId: facebookId, ref_username: "ZGF2aWRkYW5n"}
       json = JSON.parse(response)
